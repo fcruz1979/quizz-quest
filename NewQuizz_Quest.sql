33eea9f1075305ffcd9d3166106642225ba1c53c
FCruz <fcruz19790525@gmail.com>
2024-03-26 21:38:05 -0300

    NewQuizz_Quest.sql

---
+++ NewQuizz_Quest.sql
@@ -0,0 +1,6 @@
+SELECT * FROM perguntas;
+SELECT * FROM perguntas WHERE id = 1;
+SELECT * FROM perguntas WHERE id = 54;
+SELECT * FROM perguntas WHERE id = 67;
+SELECT * FROM perguntas;
+SELECT * FROM perguntas WHERE id = 200;
